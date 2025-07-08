f4fe88c717005c012a6a35417f5f16ec4701e862
DEANCAIRA <dorenth.dean@gmail.com>
2025-07-08 16:02:06 +0700

    Update app new.py

--- app new.py
+++ app new.py
@@ -154,4 +154,4 @@ else:
     st.info("👈 Upload your `cleaned_unique_customers.csv` to begin.")
 
 st.markdown("---")
-st.markdown("© 2025 Dorenth | Phrase-Level Persona Engine", unsafe_allow_html=True)
+st.markdown("© 2025 Dorenth | Made using Python 🐍", unsafe_allow_html=True)
